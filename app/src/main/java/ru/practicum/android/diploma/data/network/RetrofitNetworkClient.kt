a6de3dd45b28ca127a7bfba42f729147d8f35003
Guess56 <Guess56@yandex.ru>
2024-12-12 14:25:48 +0500

    Diploma-6: исправлено замечание связанное с обработкой исключения

--- app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
@@ -3,9 +3,11 @@ package ru.practicum.android.diploma.data.network
 import android.content.Context
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import ru.practicum.android.diploma.data.dto.Response
 import ru.practicum.android.diploma.data.dto.VacancySearchRequest
 import ru.practicum.android.diploma.util.isConnected
+import java.io.IOException
 
 class RetrofitNetworkClient(
     private val hhApi: HhApi,
@@ -16,9 +18,10 @@ class RetrofitNetworkClient(
         const val BAD_REQUEST = 400
         const val INTERNAL_SERVER_ERROR = 500
         const val NO_CONNECT = -1
+        const val INCORRECT_VACANCY = 404
     }
-    override suspend fun doRequest(dto: Any): Response {
 
+    override suspend fun doRequest(dto: Any): Response {
         if (!isConnected(context) || dto !is VacancySearchRequest) {
             return when {
                 !isConnected(context) -> Response().apply { resultCode = NO_CONNECT }
@@ -29,6 +32,10 @@ class RetrofitNetworkClient(
             try {
                 val response = hhApi.search(dto.expression)
                 response.apply { resultCode = SUCCESS_REQUEST }
+            } catch (e: IOException) {
+                Response().apply { resultCode = INTERNAL_SERVER_ERROR }
+            } catch (e: HttpException) {
+                Response().apply { resultCode = INCORRECT_VACANCY }
             } catch (e: Throwable) {
                 Response().apply { resultCode = INTERNAL_SERVER_ERROR }
             }
