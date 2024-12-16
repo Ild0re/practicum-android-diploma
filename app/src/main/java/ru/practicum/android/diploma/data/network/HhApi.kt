82201f5b79793f6a27735d8cd4a17df52d1d9c14
Ildar <nuruda@yandex.ru>
2024-12-16 11:11:45 +0300

    Исправлены ошибки детект

--- app/src/main/java/ru/practicum/android/diploma/data/network/HhApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HhApi.kt
@@ -2,13 +2,14 @@ package ru.practicum.android.diploma.data.network
 
 import retrofit2.http.GET
 import retrofit2.http.Headers
-import retrofit2.http.Query
 import retrofit2.http.QueryMap
+import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.data.dto.VacancyResponse
 
 interface HhApi {
+
     @Headers(
-        "Authorization: Bearer APPLUN1VS0I1RP6RKU9EEJ7A9KUBSR1TUOIV5SLPN8DIHNC72184FPTQ5NPH1CPA",
+        "Authorization: Bearer ${BuildConfig.HH_ACCESS_TOKEN}",
         "HH-User-Agent: job_request (gamu@bk.ru)"
     )
     @GET("/vacancies")
