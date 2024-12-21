25eade61f3b4c57c9a7a4d1e505b0174707a1d6f
Investbankir <leopardo4174@ya.ru>
2024-12-21 22:05:18 +0500

    Diploma_79

--- app/src/main/java/ru/practicum/android/diploma/ui/main/MainViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/main/MainViewHolder.kt
@@ -45,18 +45,20 @@ class MainViewHolder(private val binding: VacancyItemBinding, private val contex
     @SuppressLint("SetTextI18n")
     private fun getPrice(item: Vacancy) {
         if (item.salaryFrom == "null" && item.salaryTo == "null") {
-            binding.tvItemPrice.text = context.resources.getString(R.string.no_salary)
+            binding.tvItemPrice.text = context.resources.getString(R.string.no_salary).replaceFirstChar { it.lowercase() }
         } else if (item.salaryFrom != "null" && item.salaryTo == "null") {
             binding.tvItemPrice.text =
                 "От ${formatNumberWithSpaces(item.salaryFrom)} ${getCurrencySymbol(item.salaryCurrency)}"
+                    .replaceFirstChar { it.lowercase() }
         } else if (item.salaryFrom == "null" && item.salaryTo != "null") {
             binding.tvItemPrice.text =
                 "До ${formatNumberWithSpaces(item.salaryTo)} ${getCurrencySymbol(item.salaryCurrency)}"
+                    .replaceFirstChar { it.lowercase() }
         } else {
             binding.tvItemPrice.text =
                 "От ${formatNumberWithSpaces(item.salaryFrom)} до ${formatNumberWithSpaces(item.salaryTo)} ${
                     getCurrencySymbol(item.salaryCurrency)
-                }"
+                }".replaceFirstChar { it.lowercase() }
         }
     }
 
