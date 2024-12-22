36d0cd13309c9c36952b223b1705fdc9b0442145
Guess56 <Guess56@yandex.ru>
2024-12-23 02:35:12 +0500

    diploma 86 - добавлены 3 текствотчера для каждого Ет и обработка кнопок очистки полей а также нажатие на кнопку БезЗП

--- app/src/main/java/ru/practicum/android/diploma/ui/FilterSettingFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/FilterSettingFragment.kt
@@ -2,9 +2,12 @@ package ru.practicum.android.diploma.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -28,6 +31,8 @@ class FilterSettingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupTextWatcher()
+        setupEventHandlers()
 
         binding.backArrow.setOnClickListener {
             findNavController().popBackStack()
@@ -38,6 +43,114 @@ class FilterSettingFragment : Fragment() {
             }
         }
     }
+
+    private fun setupTextWatcher() {
+        val salaryTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // в данный момент не используется
+            }
+
+            override fun onTextChanged(p0: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!p0.isNullOrEmpty()) {
+                    binding.imageClear.isVisible = true
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // в данный момент не используется
+            }
+        }
+        binding.etSalaryHint.addTextChangedListener(salaryTextWatcher)
+        val industryTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // в данный момент не используется
+            }
+
+            override fun onTextChanged(p0: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!p0.isNullOrEmpty()) {
+                    binding.etIndustry.setEndIconDrawable(R.drawable.main_clear_icon)
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // в данный момент не используется
+            }
+        }
+        binding.etIndustryHint.addTextChangedListener(industryTextWatcher)
+        val workingPlaceTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // в данный момент не используется
+            }
+
+            override fun onTextChanged(p0: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!p0.isNullOrEmpty()) {
+                    binding.etWorkingPlace.setEndIconDrawable(R.drawable.main_clear_icon)
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // в данный момент не используется
+            }
+        }
+        binding.etWorkingPlaceHint.addTextChangedListener(workingPlaceTextWatcher)
+    }
+
+    private fun setupEventHandlers() {
+        binding.imageClear.setOnClickListener {
+            binding.etSalaryHint.setText("")
+            if (binding.etSalaryHint.text.isNullOrEmpty()) {
+                binding.imageClear.isVisible = false
+                binding.etSalaryHint.isFocusable = false
+            }
+        }
+        binding.etWorkingPlace.setEndIconOnClickListener {
+            binding.etWorkingPlaceHint.setText("")
+            if (binding.etWorkingPlaceHint.text.isNullOrEmpty()) {
+                binding.etWorkingPlace.setEndIconDrawable(R.drawable.filter_arrow_right_icon)
+                binding.etWorkingPlaceHint.isFocusable = false
+            }
+        }
+        binding.etIndustry.setEndIconOnClickListener {
+            binding.etIndustryHint.setText("")
+            if (binding.etIndustryHint.text.isNullOrEmpty()) {
+                binding.etIndustry.setEndIconDrawable(R.drawable.filter_arrow_right_icon)
+                binding.etIndustryHint.isFocusable = false
+            }
+        }
+        binding.salaryClose.setOnClickListener {
+            binding.salaryClose.setCompoundDrawablesWithIntrinsicBounds(
+                0,
+                0,
+                R.drawable.filter_square_activated_icon,
+                0
+            )
+        }
+        binding.reset.setOnClickListener {
+            binding.etWorkingPlaceHint.setText("")
+            binding.etIndustryHint.setText("")
+            binding.etIndustryHint.setText("")
+        }
+
+        binding.etSalaryHint.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // пока не используется
+            }
+            false
+        }
+        binding.etWorkingPlaceHint.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // пока не используется
+            }
+            false
+        }
+        binding.etIndustryHint.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // пока не используется
+            }
+            false
+        }
+    }
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationView).isVisible = false
