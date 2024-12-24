c7d7b9baa8bda4de8cc2638d5d92418d206ad3e7
Guess56 <Guess56@yandex.ru>
2024-12-24 11:46:40 +0500

    diploma 94 - добавлено отображение РВ на заглушке с отображением кнопки выбрать при нажатии на элемент списка

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/industry/ChoosingIndustryFragment.kt
@@ -0,0 +1,86 @@
+package ru.practicum.android.diploma.ui.industry
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.koin.core.component.getScopeId
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentChoosingIndustryBinding
+import ru.practicum.android.diploma.domain.models.Industry
+
+class ChoosingIndustryFragment : Fragment() {
+    private var _binding: FragmentChoosingIndustryBinding? = null
+    private var adapter = IndustryAdapter()
+    val binding: FragmentChoosingIndustryBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentChoosingIndustryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backArrow.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        // временно для подключения РВ
+        val industry = arrayListOf(
+            Industry("1", "Авиаперевозки", false),
+            Industry("2", "Автошкола", false),
+            Industry("3", "Агрохимия", false),
+        )
+        adapter.updateItems(industry)
+        binding.rvIndustry.adapter = adapter
+        binding.rvIndustry.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        adapter.notifyDataSetChanged()
+
+        adapter.onItemClickListener = IndustryViewHolder.OnItemClickListener { item ->
+            updateList(item, industry)
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationView).isVisible = false
+    }
+
+    private fun updateList(item: Industry, industry: ArrayList<Industry>) {
+        for (i in industry) {
+            if (i.id.equals(item.id)) {
+                item.click = !item.click
+                val itemIndustry = listOf(Industry(item.id, item.name, item.click))
+                if (itemIndustry.isEmpty()) {
+                    binding.btApply.isVisible = false
+                } else {
+                    binding.btApply.isVisible = true
+                    clickApply(itemIndustry)
+                }
+                adapter.updateItems(itemIndustry)
+                adapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    private fun clickApply(item: List<Industry>) {
+        binding.btApply.setOnClickListener {
+            for (i in item) {
+                binding.etSearch.setText(i.name)
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/industry/IndustryAdapter.kt
@@ -0,0 +1,54 @@
+package ru.practicum.android.diploma.ui.industry
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.domain.models.Industry
+
+class IndustryAdapter : RecyclerView.Adapter<IndustryViewHolder>() {
+
+    var onItemClickListener: IndustryViewHolder.OnItemClickListener? = null
+
+    private var items: List<Industry> = emptyList()
+    fun updateItems(newItems: List<Industry>) {
+        val oldItems = items
+        val diffResult = DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+            override fun getOldListSize(): Int {
+                return oldItems.size
+            }
+
+            override fun getNewListSize(): Int {
+                return newItems.size
+            }
+
+            override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                return oldItems[oldItemPosition].id == newItems[newItemPosition].id
+            }
+
+            override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+                return oldItems[oldItemPosition] == newItems[newItemPosition]
+            }
+
+        })
+        items = newItems.toMutableList()
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IndustryViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.industry_item, parent, false)
+        return IndustryViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: IndustryViewHolder, position: Int) {
+        holder.bind(items[position],onItemClickListener = onItemClickListener)
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+
+    }
+}
