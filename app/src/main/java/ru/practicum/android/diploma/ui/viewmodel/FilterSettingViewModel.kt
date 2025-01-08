71d0b7a3e4660c9c773d99c6c6b2ce563b23154e
Ildar <nuruda@yandex.ru>
2025-01-08 17:13:12 +0300

    Изменен регион и страна

--- app/src/main/java/ru/practicum/android/diploma/ui/viewmodel/FilterSettingViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/viewmodel/FilterSettingViewModel.kt
@@ -26,21 +26,21 @@ class FilterSettingViewModel(
         loadFilter()
     }
 
-    private var countryList: List<Area>? = null
-    private var areaList: List<Area>? = null
-    private var industryList: List<Industry>? = null
+    private var filter: Filter = Filter(null, null, null, null, false)
+    private var countryObject: Area? = null
+    private var areaObject: Area? = null
+    private var industryObject: Industry? = null
 
     private val filterState = MutableLiveData<Filter>()
 
     fun getFilter(): LiveData<Filter> = filterState
 
     fun loadFilter() {
-        getCountryIds()
-        getIndustriesIds()
         viewModelScope.launch {
-            val filter = filterInteractor.getFilter()
+            val filterObject = filterInteractor.getFilter()
+            filter = filterInteractor.getFilter()
             delay(SEARCH_DEBOUNCE_DELAY)
-            postFilter(filter)
+            postFilter(filterObject)
         }
     }
 
@@ -49,34 +49,24 @@ class FilterSettingViewModel(
     }
 
     fun saveFilter(country: String?, area: String?, scope: String?, salary: String?, noSalary: Boolean): Filter {
-        val countryObject = countryList?.find { it.name == country }
-        if (countryObject != null) {
-            getRegionId(countryObject)
+        countryObject = if (country == null) {
+            null
+        } else {
+            filter.country
         }
-        val industryObject = industryList?.find { it.name == scope }
-        val areaObject = areaList?.find { it.name == area }
+        areaObject = if (area == null) {
+            null
+        } else {
+            filter.area
+        }
+        industryObject = if (scope == null) {
+            null
+        } else {
+            filter.scope
+        }
         return filterInteractor.saveFilter(Filter(countryObject, areaObject, industryObject, salary, noSalary))
     }
 
-    private fun getCountryIds() {
-        viewModelScope.launch {
-            referencesInteractor.getCountries().collect { pair -> countryList = pair.first }
-        }
-    }
-
-    private fun getIndustriesIds() {
-        viewModelScope.launch {
-            referencesInteractor.getIndustries().collect { pair ->
-                industryList = pair.first
-            }
-        }
-    }
-
-    private fun getRegionId(country: Area) {
-        viewModelScope.launch {
-            referencesInteractor.getRegions(country).collect { pair -> areaList = pair.first }
-        }
-    }
     fun updateFilter(bottom: Boolean) {
         filterInteractor.updateFilter(FilterField.ONLY_WITH_SALARY, bottom)
     }
